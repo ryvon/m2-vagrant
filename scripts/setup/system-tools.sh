4d57bed62fe7d828714d348a64b6b0a2431173cd
Erik Perri <erik.perri@gmail.com>
2019-08-07 21:20:26 -0400

    Added node

--- scripts/setup/system-tools.sh
+++ scripts/setup/system-tools.sh
@@ -3,8 +3,15 @@ set -e
 
 [[ -z ${SETUP_LOG} ]] && exit 1
 
-echo Installing system tools
+echo "Installing system tools"
 {
   apt-get -y install apt-transport-https bash-completion ca-certificates curl \
     git lsb-release unzip zip
+
+  echo "Installing nodejs"
+  curl -sL https://deb.nodesource.com/setup_8.x | bash -
+  apt-get -y install nodejs
+
+  echo "Installing grunt-cli and gulp globally"
+  npm install --loglevel=error -g grunt-cli gulp
 } >>"${SETUP_LOG}"
