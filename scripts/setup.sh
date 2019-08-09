41588de7e2d29ee0f170fc5d30dd9115e84a15fa
Erik Perri <erik.perri@gmail.com>
2019-08-09 17:26:40 -0400

    Updated source comment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,26 +15,26 @@ if [[ -f ${SETUP_LOG} ]]; then
   rm "${SETUP_LOG}"
 fi
 
-# shellcheck source=scripts/setup/update.sh
+# shellcheck source=./setup/update.sh
 . "${VAGRANT_ROOT}/scripts/setup/update.sh"
 
-# shellcheck source=scripts/setup/system-tools.sh
+# shellcheck source=./setup/system-tools.sh
 . "${VAGRANT_ROOT}/scripts/setup/system-tools.sh"
 
-# shellcheck source=scripts/setup/apache.sh
+# shellcheck source=./setup/apache.sh
 . "${VAGRANT_ROOT}/scripts/setup/apache.sh"
 
-# shellcheck source=scripts/setup/mysql.sh
+# shellcheck source=./setup/mysql.sh
 . "${VAGRANT_ROOT}/scripts/setup/mysql.sh"
 
-# shellcheck source=scripts/setup/php.sh
+# shellcheck source=./setup/php.sh
 . "${VAGRANT_ROOT}/scripts/setup/php.sh"
 
-# shellcheck source=scripts/setup/mailhog.sh
+# shellcheck source=./setup/mailhog.sh
 . "${VAGRANT_ROOT}/scripts/setup/mailhog.sh"
 
-# shellcheck source=scripts/setup/composer.sh
+# shellcheck source=./setup/composer.sh
 . "${VAGRANT_ROOT}/scripts/setup/composer.sh"
 
-# shellcheck source=scripts/setup/magento.sh
+# shellcheck source=./setup/magento.sh
 . "${VAGRANT_ROOT}/scripts/setup/magento.sh"
